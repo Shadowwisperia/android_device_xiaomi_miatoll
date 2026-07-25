c4984e24778e633eb5726e173c2df1faa7533c08
Shadowwisperia <sreerajnd9@gmail.com>
2026-07-25 09:23:08 +0000

    miatoll: Update MiuiCamera Paths

--- device.mk
+++ device.mk
@@ -375,4 +375,4 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
 
 # Miui-Camera
-$(call inherit-product-if-exists, vendor/xiaomi/miuicamera-miatoll/MiuiCamera.mk)
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-miatoll/device.mk)
